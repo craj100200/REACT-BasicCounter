13db66054338143867dc8d24f1a9b2f7387ce94d
creaj100200@gmail.com <creaj100200@gmail.com>
2023-06-11 17:42:32 +0000

    Updates

---
+++ precache-manifest.79e84107b1d358ededf05d3fb481167f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "809c95f911e6bc55fe52",
+    "url": "/static/css/main.24f24985.chunk.css"
+  },
+  {
+    "revision": "809c95f911e6bc55fe52",
+    "url": "/static/js/main.809c95f9.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "3eca10627046b991fa818b76487467e1",
+    "url": "/index.html"
+  }
+];
